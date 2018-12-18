a7d2a96ae1cc01aafbce28685b77e9468b940451
Harpal Gupta <harpal.gupta@gmail.com>
2018-12-18 16:19:48 +0000

    added controller for fetching url data

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,9 +1,24 @@
-const http = require('http');
+const https = require('https');
 
 exports.getUrl = (req, res, url) => {
-  http.get(url);
-
-
+  https.get(url, (output) => {
+    let rawData = '';
+    output.on('data', (data) => {
+      rawData += data;
+    });
+    output.on('end', () => {
+      const rawDataArray = rawData.split('\n');
+      //   let matches = '';
+      //   matches += rawData.match(/<a\s/);
+      console.log(rawDataArray);
+      const newArray = rawDataArray.reduce((acc, entry) => {
+        if (entry.match(/<a/)) {
+          return acc;
+        }
+      }, []);
+      console.log(newArray);
+    });
+  });
   console.log('in get url');
   res.write('HI');
   res.end();
